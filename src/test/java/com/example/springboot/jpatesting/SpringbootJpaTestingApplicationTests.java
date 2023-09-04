15627ed7599a61aa11e1cb21624aa9803c0f702c
Kalana Gunaweera <kalanagunaweera94@gmail.com>
2023-09-04 17:16:58 +0530

    first commmit

---
+++ src/test/java/com/example/springboot/jpatesting/SpringbootJpaTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot.jpatesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpaTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
